9133fb16850a01c8ae6c6f2ab3544edc3ff76cf3
yahiakr <yahiakerim@gmail.com>
2020-03-23 19:21:58 +0100

    added deployment configuration

--- orders-microservice/src/controllers/v0/index.router.ts
+++ orders-microservice/src/controllers/v0/index.router.ts
@@ -3,7 +3,7 @@ import { OrderRouter } from './order/routes/order.router';
 
 const router: Router = Router();
 
-router.use('/order', OrderRouter);
+router.use('/orders', OrderRouter);
 
 router.get('/', async (req: Request, res: Response) => {    
     res.send(`V0`);
